850cfbed2561beace0fe5ae4d5d12f348be82cec
ben <beni_nellson@hotmail.com>
2019-08-24 14:48:22 +0200

    Final commit, added functionality

--- NotesReactNative/app/components/Main.js
+++ NotesReactNative/app/components/Main.js
@@ -1,55 +1,90 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
   TextInput,
-  StatusBar,
   TouchableOpacity,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Note from './Note';
 
-const Main = () => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}> Notes React Native  </Text>
+export default class Main extends React.Component {
+
+  constructor(props){
+
+    super(props);
+    this.state = {
+      noteArray: [],
+      noteText: '',
+    }  
+  }
+  
+  render(){
+    let notes = this.state.noteArray.map((val,key)=>{
+      return <Note key={key} keyval={key} val={val}
+      deleteMethod={ ()=> this.deleteNote(key)}/>
+    });
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}> Notes React Native  </Text>
+        </View>
+  
+        <ScrollView style={styles.scrollContainer}>
+          {notes}
+        </ScrollView>
+  
+        <View style ={styles.footer}>
+          <TextInput style={styles.textInput}
+          onChangeText={(noteText) => this.setState({noteText})}
+          value={this.state.noteText}
+          placeholder='Add note...'
+          placeholderTextColor='white'
+          underlineColorAndroid = 'transparent'>
+  
+          </TextInput>
+        </View>
+        <TouchableOpacity onPress={this.addNote.bind(this)} style={styles.addButton}>
+          <Text style ={styles.addButtonText}>+</Text>
+        </TouchableOpacity>
       </View>
+    );
+  }
+   
+  //TODO, add Realm, save
+  addNote(){
+    console.log("addnote pressed")
 
-      <ScrollView style={styles.scrollContainer}>
+    if(this.state.noteText){
+      
+      var d = new Date();
 
-      </ScrollView>
+      this.state.noteArray.push({
+        'date': d.getFullYear() +
+        "/" + (d.getMonth() + 1) + 
+        "/" + d.getDate(),
+        'note': this.state.noteText
+      });
+      this.setState({noteArray: this.state.noteArray})
+      this.setState ({noteText: ''});
+    }
+  }
 
-      <View style ={styles.footer}>
-        <TextInput style={styles.textInput}
-        placeholder='note'
-        placeholderTextColor='white'
-        underlineColorAndroid = 'transparent'>
+  deleteNote (key){
+    this.state.noteArray.splice(key,1);
+    this.setState({noteArray: this.state.noteArray})
+  }
+}
 
-        </TextInput>
-      </View>
-      <TouchableOpacity style= {styles.addButton}>
-        <Text style ={styles.addButtonText}>+</Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
 
 const styles = StyleSheet.create({
   container: {
     flex:1,
   },
   header: {
-    backgroundColor: '#E91E63',
+    backgroundColor: '#2980b9',
     alignItems: 'center',
     justifyContent: 'center',
     borderBottomWidth: 10,
@@ -58,7 +93,7 @@ const styles = StyleSheet.create({
   headerText: {
     color: 'white',
     fontSize: 18,
-    padding: 26,
+    padding: 20,
   },
   scrollContainer: {
     flex: 1,
@@ -74,21 +109,19 @@ const styles = StyleSheet.create({
   textInput: {
     alignSelf: 'stretch',
     color: '#fff',
-    padding: 20,
+    padding: 10,
     backgroundColor: '#252525',
     borderTopWidth: 2,
     borderTopColor: '#ededed',
   },
-
-
   addButton: {
     position: 'absolute',
     zIndex: 11,
     right: 20,
-    bottom: 90,
-    backgroundColor: '#E91E63',
-    width: 90,
-    height: 90,
+    bottom: 70,
+    backgroundColor: '#2980b9',
+    width: 50,
+    height: 70,
     borderRadius: 50,
     alignItems: 'center',
     justifyContent: 'center',
@@ -96,10 +129,6 @@ const styles = StyleSheet.create({
   },
   addButtonText: {
     color: '#fff',
-    fontSize: 24,
+    fontSize: 34,
   },
 });
-
-
-
-export default Main;
